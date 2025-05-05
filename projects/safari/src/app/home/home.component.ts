7ebee5e20cab6b050cccf8113d01b58c3991c3dc
Shiva Krishna <pallewarshiva@gmail.com>
2025-05-06 01:22:57 +0530

    added destinations

--- projects/safari/src/app/home/home.component.ts
+++ projects/safari/src/app/home/home.component.ts
@@ -1,13 +1,38 @@
-import { Component, ElementRef, model, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, model, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.less',
 })
-export class HomeComponent{
+export class HomeComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    setInterval(() => {
+      const container = this.destinationsContainer.nativeElement;
+      const currentPos = container.scrollLeft;
 
+      if (currentPos >= container.scrollWidth / 2) {
+        container.scrollLeft = 0;
+        container.scrollLeft += 100;
+      }
+    }, 2000);
+  }
 
-  
+  @ViewChild('destinationsContainer', { static: false }) destinationsContainer: any;
+
+destinations :any = [
+  {icon: 'assets/GOKARNA-DANDELI.webp' },
+  {icon: 'assets/KERALA.webp'},
+  {icon: 'assets/KASHMIR.webp'},
+  {icon: 'assets/MANALI.webp'},
+  {icon: 'assets/GOA.webp'},
+  {icon: 'assets/DARJEELING.webp'},
+  { icon: 'assets/KODAIKANAL.webp'},
+  {icon: 'assets/MEGHALAYA.webp'},
+  {icon: 'assets/OOTY.webp'},
+  {icon: 'assets/RAJASTHAN.webp'},
+]
+
 }
